c4913a79c97406deba280f4e529304d4d8464658
Georg <georghome1@gmail.com>
2022-12-19 22:08:41 +0300

    Add EX2 three number in and max out

---
+++ EX2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите первое целое число");
+string userInput1 = Console.ReadLine() ?? "";
+int number1 = int.Parse(userInput1);
+
+Console.WriteLine("Введите второе целое число");
+string userInput2 = Console.ReadLine() ?? "";
+int number2 = int.Parse(userInput2);
+
+Console.WriteLine("Введите второе целое число") ;
+string userInput3 = Console.ReadLine() ?? "";
+int number3 = int.Parse(userInput3);
+
+int max = number1;
+if(number2 > max)
+{
+    max = number2;
+}    
+else if(number3 > max);
+{
+    max = number3;
+}
+Console.WriteLine($"Первое число - {number1}");
+Console.WriteLine($"Первое число - {number2}");
+Console.WriteLine($"Первое число - {number3}");
+Console.WriteLine($"Максимальное число - {max}");
