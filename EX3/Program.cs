d465ccd08f06dc3f78b5df9024dcf53378d673b8
Georg <georghome1@gmail.com>
2022-12-19 23:06:05 +0300

    Add  EX3

---
+++ EX3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите целое число ");
+string userInput = Console.ReadLine() ?? "";
+int number = int.Parse(userInput);
+
+int num = number%2;
+//Console.WriteLine($"значение {num}");
+if(num == 0)
+{
+    Console.WriteLine($"Вы ввели число {number} - оно четное");
+}
+else 
+{
+Console.WriteLine($"Вы ввели число {number} - оно не четное");
+}
