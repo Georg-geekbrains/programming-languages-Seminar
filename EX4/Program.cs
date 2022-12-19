f20f43ab963d7947246cdc45b7a173d9b46841ea
Georg <georghome1@gmail.com>
2022-12-19 23:40:02 +0300

    Add EX4

---
+++ EX4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите целое число ");
+string userInput = Console.ReadLine() ?? "";
+int N = int.Parse(userInput);
+Console.Write($"{N} ->");
+
+int count = 2;
+
+
+while (count <= N)
+{
+    Console.Write($" {count}, ");
+    count += 2;
+    
+}
