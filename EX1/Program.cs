f0dd4822396e2935cf3dd468cf53b12170e08bf6
Georg <georghome1@gmail.com>
2022-12-19 21:17:38 +0300

    add EX1 max min numbers

---
+++ EX1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое целое число");
+string userInput1 = Console.ReadLine() ?? "";
+int number1 = int.Parse(userInput1);
+
+Console.WriteLine("Введите второе целое число");
+string userInput2 = Console.ReadLine() ?? "";
+int number2 = int.Parse(userInput2);
+
+if(number1 > number2)
+{
+    Console.WriteLine($"Максимальное число {number1}");
+    Console.WriteLine($"Минимальное число {number2}");
+}
+else 
+{
+    Console.WriteLine($"Максимальное число {number2}");
+    Console.WriteLine($"Минимальное число {number1}");
+}
