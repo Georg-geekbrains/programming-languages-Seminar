caaa36c48ffd19ed51d5957db0c1ab6b04181d26
Georg <georghome1@gmail.com>
2022-12-19 22:39:26 +0300

    add EX2

--- EX2/Program.cs
+++ EX2/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Введите первое целое число");
+﻿
+Console.WriteLine("Введите первое целое число");
 string userInput1 = Console.ReadLine() ?? "";
 int number1 = int.Parse(userInput1);
 
@@ -6,20 +7,21 @@ Console.WriteLine("Введите второе целое число");
 string userInput2 = Console.ReadLine() ?? "";
 int number2 = int.Parse(userInput2);
 
-Console.WriteLine("Введите второе целое число") ;
+Console.WriteLine("Введите третье целое число") ;
 string userInput3 = Console.ReadLine() ?? "";
 int number3 = int.Parse(userInput3);
 
 int max = number1;
+
 if(number2 > max)
 {
     max = number2;
 }    
-else if(number3 > max);
+if(number3 > max)
 {
     max = number3;
 }
 Console.WriteLine($"Первое число - {number1}");
-Console.WriteLine($"Первое число - {number2}");
-Console.WriteLine($"Первое число - {number3}");
+Console.WriteLine($"Второе число - {number2}");
+Console.WriteLine($"Третье число - {number3}");
 Console.WriteLine($"Максимальное число - {max}");
