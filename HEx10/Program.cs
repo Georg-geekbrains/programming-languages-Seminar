63e122dd27c40e1ec40da59d603c685d842a4aa3
Georg <georghome1@gmail.com>
2022-12-25 11:41:06 +0300

    add HEx10

---
+++ HEx10/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите целое трёхзначное числ:");
+string userInput = Console.ReadLine() ?? "";
+int number = int.Parse(userInput);
+
+int num2 = number % 100 / 10;
+Console.WriteLine($"{number} ->   {num2}");
