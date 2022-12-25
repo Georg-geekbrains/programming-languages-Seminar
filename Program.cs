de83b193a20f5e4301f3c15986158204d2f5b7c9
Georg-geekbrains <120080723+Georg-geekbrains@users.noreply.github.com>
2022-12-25 11:34:16 +0300

    Delete Program.cs

--- Program.cs
+++
@@ -1,6 +0,0 @@
-﻿Console.WriteLine("Введите целое трёхзначное числ:");
-string userInput = Console.ReadLine() ?? "";
-int number = int.Parse(userInput);
-
-int num2 = number % 100 / 10;
-Console.WriteLine($"{number} ->   {num2}");
