09f36477eb948aeb940e896db9c44912dd0162aa
Georg <georghome1@gmail.com>
2022-12-25 11:53:13 +0300

    add HEx15

---
+++ HEx15/Program.cs
@@ -0,0 +1,15 @@
+﻿int varA;
+
+while(true)
+{
+Console.WriteLine("Введите число от 1 до 7");
+if(int.TryParse(Console.ReadLine(),out varA))
+if(varA < 8 && varA > 0)
+break;
+Console.WriteLine("Ошибка ввода!");
+
+}
+//Console.WriteLine($"A = {varA}");
+if(varA == 6 | varA == 7) Console.WriteLine($"{varA} --> да");
+else
+    Console.WriteLine($"{varA} --> нет");
