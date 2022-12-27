8af1755540f566f7858b716bdd0ffc19926b71e2
Georg <georghome1@gmail.com>
2022-12-27 21:39:02 +0300

    solving the task 19

--- HW19/Program.cs
+++ HW19/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход пятизначное число
+ и проверяет, является ли оно палиндромом.*/
+
+
+int num;
+
+while(true)
+{
+        Console.Write("Введите пятизначное число: ");
+
+        try
+        {
+            num = Convert.ToInt32(Console.ReadLine()?? "");
+            if(num < 99999 && num >9999 )
+            break;
+        }
+        catch(Exception exc) 
+        {
+            Console.WriteLine($"Ошибка ввода! {exc.Message}"); 
+        }
+}
+int firstNumberLeft = num / 10000;
+int secondNumberLeft = num /1000 % 10;
+int firstNumberRight = num % 10;
+int secondNumberRight = num % 100 / 10;
+
+
+if(firstNumberLeft == firstNumberRight && secondNumberLeft == secondNumberRight) 
+{
+        Console.WriteLine($"{firstNumberLeft}");
+        Console.WriteLine($"{secondNumberLeft}");
+        Console.WriteLine($"{firstNumberRight}");
+        Console.WriteLine($"{secondNumberRight}");
+        Console.WriteLine($"Число {num} -> палиндром");   
+}
+    else
+    {
+        Console.WriteLine($"{firstNumberLeft}");
+        Console.WriteLine($"{secondNumberLeft}");
+        Console.WriteLine($"{firstNumberRight}");
+        Console.WriteLine($"{secondNumberRight}");
+        Console.WriteLine($"Число {num} -> не палиндром");
+    }
