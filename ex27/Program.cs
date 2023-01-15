7371fd08bdc33e2eff0a5cad1c0a5cbdca72646b
Georg <georghome1@gmail.com>
2023-01-15 19:57:05 +0300

    add ex27

---
+++ ex27/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдает сумму
+//цифр в числе 452 -> 11
+Console.Clear();
+int num = GetNumberFromUser("Введите число: ", "Ошибка ввода");
+int Sum = GetSumDig(num);
+Console.Write($"{num} -> {Sum}");
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+int GetSumDig(int number)
+{
+    int sum = 0;
+    while ( number > 0)
+    {
+        int t = number % 10;
+        sum += t;
+        number = number/10;
+    }
+    return sum;
+}
