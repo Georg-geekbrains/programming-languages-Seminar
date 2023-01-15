4ff8d45c7624c12c740da9b65e81e81ee5f041b4
Georg <georghome1@gmail.com>
2023-01-15 19:12:48 +0300

    add ex 26

---
+++ ex26/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 26: Напишите программу, которая
+// принимает на вход число и выдает количество цифр в числе
+
+Console.Clear();
+int num = GetUserFromNumber("Введите число: ", "Ошибка ввода");
+int CountNumber = GetCountNumber(num);
+Console.Write($"{num} -> {CountNumber} ");
+
+int GetUserFromNumber(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.Write(errorMessage);
+    }
+}
+int GetCountNumber(int number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+        number = number / 10;
+        count ++;
+    }
+        return count;
+    
+}
