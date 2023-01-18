63a6ded9389d346b02215816026e5ad20cd0cb6f
Georg <georghome1@gmail.com>
2023-01-18 23:54:46 +0300

    add ex29

--- ex29/Program.cs
+++ ex29/Program.cs
@@ -1,21 +1,6 @@
 ﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов
 // и выводит их на экран.
 
-//int [] myarray;
-//myarray =new int [8];
-//myarray[1] = 3;
-
-
-/*int[] array = new int[8];
-Random myRandom = new Random();
-
-Console.WriteLine("Вывод с помощью for");
-for (int i = 0; i < array.Length; i++)
-{
-    array[i] = myRandom.Next(0, 20);
-    Console.Write(" {0} ", array[i] );
-}*/
-
 Console.Clear();
 //int num = GetNumberFromUser("Введите число: ", "Ошибка ввода");
 int GetNumberFromUser(string message, string errorMessage)
@@ -31,18 +16,16 @@ int GetNumberFromUser(string message, string errorMessage)
 }
 
 //формирование массива
-//int[] newArray(string message)
-
+ 
     int[] array = new int[8];
     for (int i = 0; i < 8; i++)
     {
         int n = GetNumberFromUser("Введите число: ", "Ошибка ввода");
         array[i] = n;
     }
-
-
-
+ 
 //Вывод массива
+
 Console.Write("[");
 for (int i = 0; i < array.Length; i++)
 {
