a95ab773ac1dc1c20d41640144b7370027bda086
Georg <georghome1@gmail.com>
2023-01-19 16:00:23 +0300

    add ex 29

--- ex29/Program.cs
+++ ex29/Program.cs
@@ -1,8 +1,12 @@
 ﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов
 // и выводит их на экран.
 
+
+
 Console.Clear();
-//int num = GetNumberFromUser("Введите число: ", "Ошибка ввода");
+int[] array = GetUserArray(8);
+PrintArray(array);
+
 int GetNumberFromUser(string message, string errorMessage)
 {
     while (true)
@@ -14,27 +18,32 @@ int GetNumberFromUser(string message, string errorMessage)
         Console.WriteLine(errorMessage);
     }
 }
-
-//формирование массива
- 
-    int[] array = new int[8];
-    for (int i = 0; i < 8; i++)
+int[] GetUserArray(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
     {
         int n = GetNumberFromUser("Введите число: ", "Ошибка ввода");
-        array[i] = n;
+        arr[i] = n;
     }
- 
-//Вывод массива
-
-Console.Write("[");
-for (int i = 0; i < array.Length; i++)
-{
-
-    Console.Write($" {array[i]} ");
-
-    if (i < array.Length - 1)
-    {
-        Console.Write(", ");
-    }
+    return arr;
 }
-Console.Write("]");
+void PrintArray(int [] printArray)
+{
+    //Вывод массива
+
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+
+        Console.Write($" {array[i]} ");
+
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+
+
