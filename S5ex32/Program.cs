4f0bd91c934f748f69e0bfd1c01b109132efc630
Georg <georghome1@gmail.com>
2023-01-20 22:37:48 +0300

    add new ex

---
+++ S5ex32/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 32. Напишите программу замены элементов массива. 
+//Положительные элементы замените на соответсвующие отрицательные, и наоборот
+
+ /* В данном случае исходный массив не сохраняется. Поэтому последовательность вывода. Выводим первый
+массив, затем меняем и выводим измененный массив. Пояснения Семинар 5 time cod 1:05;00
+Доп задание написать так чтобы сохранилось оба массива*/
+
+
+
+
+Console.Clear();
+int[] array = GetArray(10, -9, 10);
+Console.WriteLine($"[ {String.Join(", ", array)} ]");
+GetNewArray(array);
+Console.WriteLine($"[ {String.Join(", ", array)} ]");
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue);
+    }
+    return result;
+}
+
+void GetNewArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = -arr[i];
+    }
+
+}
