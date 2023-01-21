e4f10a3aa821421ee199f8448e4044380a3ca798
Georg <georghome1@gmail.com>
2023-01-21 20:47:12 +0300

    add home ex36

---
+++ S5hex36/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Задача 36: Задайте одномерный массив, заполненный случайными числами.
+Найдите сумму элементов, стоящих на нечётных позициях.*/
+Console.Clear();
+
+int[] array = GetNewArray(10, 1, 10);
+int sumNegativePosition = NegativePosition(array);
+
+Console.WriteLine($"[ {String.Join(", ", array)} ]");
+Console.WriteLine($"{sumNegativePosition}");
+
+int[] GetNewArray(int size, int minValue, int maxValue)
+{
+    int[] arr = new int[size];
+    var rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(minValue, maxValue);
+    }
+    return arr;
+}
+int NegativePosition(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 == 1) sum = sum + arr[i];
+    }
+    return sum;
+}
